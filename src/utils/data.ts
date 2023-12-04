ee476d04748c03aaad3728197c0c8b74b7c94c9e
Juanmanms <juanmanms@gmail.com>
2023-12-04 18:12:42 +0100

    first

---
+++ src/utils/data.ts
@@ -0,0 +1,54 @@
+const services = [
+    {
+        title: 'Auditorias',
+        description:
+          'Identificamos y corregimos posibles mejoras en usabilidad, velocidad y seguridad, asegurando un rendimiento óptimo. Fortalece la confianza del cliente y maximiza conversiones para impulsar tu éxito comercial.',
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`
+      },
+      {
+        title: 'Manteniento',
+        description: `Optimiza tu tienda online con nuestro servicio de mantenimiento en PrestaShop. Mantén un rendimiento óptimo, seguridad robusta y actualizaciones continuas para que puedas concentrarte en hacer crecer tu negocio mientras nosotros cuidamos de tu plataforma.`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`
+      },
+      {
+        title: 'Soporte para modulos',
+        description:
+          'Potencia tu tienda con nuestro servicio de actualización y configuración de módulos en PrestaShop. Garantizamos un rendimiento óptimo y la implementación eficiente de nuevas funciones, permitiéndote aprovechar al máximo tu plataforma sin preocupaciones técnicas.',
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`
+      },
+      {
+        title: 'Personalización unica',
+        description: `Destaca con configuraciones personalizadas en PrestaShop. Nuestro servicio te ofrece soluciones a medida, asegurando que tu tienda se adapte perfectamente a tus necesidades específicas, proporcionando una experiencia única para tus clientes y un diferencial competitivo para tu negocio.`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`
+      },
+      {
+        title: 'Consultoria de mejoras',
+        description: `Actualiza tu estrategia comercial con nuestra consultoría de mejoras en PrestaShop. Analizamos a fondo tu tienda en línea y proponemos soluciones personalizadas para optimizar la usabilidad, aumentar la conversión y mejorar la experiencia del usuario, proporcionándote un camino claro hacia el crecimiento sostenible de tu negocio.`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`
+      },
+      {
+        title: 'Mejora de metricas de posicionamiento',
+        description: `Mejora la velocidad de tu tienda. Optimizamos métricas clave, como Page Speed, y aplicamos soluciones eficientes para aumentar la eficacia de tu sitio, potenciando así la satisfacción del usuario y las conversiones.`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`
+      },
+      {
+        title: 'Estudio SEO',
+        description: `Potencia tu visibilidad. Analizamos y optimizamos tu sitio web para mejorar tu posicionamiento en los motores de búsqueda, asegurando un mayor alcance y relevancia en línea.`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`
+      },
+      {
+        title: 'Propuesta/implantación SEM',
+        description: `Maximiza tu presencia digital con nuestra propuesta e implantación SEM. Aumentamos la visibilidad de tu negocio a través de estrategias efectivas de publicidad en buscadores, garantizando resultados inmediatos y un retorno de inversión significativo.`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`
+      },
+];
+
+export { services };
